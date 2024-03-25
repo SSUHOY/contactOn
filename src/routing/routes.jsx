7a50ed6fe1a0d9b3b9c150060e7bd13dfa4314c8
Semyon <125258352+SSUHOY@users.noreply.github.com>
2024-03-25 20:13:02 +0700

    refactors before deploy

--- src/routing/routes.jsx
+++ src/routing/routes.jsx
@@ -6,16 +6,18 @@ import UserProfile from "../pages/profilePage";
 import MessagesUI from "../pages/messagesPage";
 import MessagesInBox from "../pages/messagesInBox";
 import Friends from "../pages/friendsPage";
+import Main from "../pages/MainPage";
+import Auth from "../pages/AuthPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<Main />} />
       <Route path="users">
         <Route path=":id" element={<UserProfile />} />
       </Route>
-      <Route path="/login" element={<AuthPage />} />
-      <Route path="/register" element={<AuthPage />} />
+      <Route path="/login" element={<Auth />} />
+      <Route path="/register" element={<Auth />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/profile" element={<AuthUserProfile />} />
         <Route path="/friends" element={<Friends />} />
